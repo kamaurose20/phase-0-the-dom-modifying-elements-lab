18baedaba5c2559d86eaec96aa2a834f65c5dc17
rosekamau <kamaurose236@gmail.com>
2024-07-17 14:33:25 +0300

    The dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+const element = document.getElementById('main')
+
+element.remove()
+
+const newHeader = document.createElement("H1")
+
+newHeader.id = 'victory'
+
+newHeader.textContent = "German Santos is the champion";
